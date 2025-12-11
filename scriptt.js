33d205835bd84a58b39ac6e162edf26b75601446
tacdoldaerenjake2005-lab <tacdoldaerenjake2005@gmail.com>
2025-12-11 08:10:30 +0800

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,36 @@
+const menubar = document.querySelector('#menu');
+const Navbar = document.querySelector('.navbar');
+menubar.onclick=()=>{
+    menubar.classList.toggle('bx-x');
+    Navbar.classList.toggle('active')
+}
+const toggleButton = document.getElementById('dark-mode-toggle');
+        const body = document.body;
+
+        toggleButton.addEventListener('click', () => {
+            body.classList.toggle('dark-mode');
+            const isDarkMode = body.classList.contains('dark-mode');
+            toggleButton.textContent = isDarkMode ? 'Toggle Dark Mode' : 'Toggle Light Mode';
+        });
+const section=document.querySelectorAll('section');
+const navlink = document.querySelectorAll('header nav a')
+window.onscroll = ()=>{
+    section.forEach(sec=>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+        if(top>offset && top < offset + height){
+            sec.classList.add('start-animation');
+            navlink.forEach(links=>{
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*='+id+']').classList.add('active')
+              
+            })
+        }
+    })
+    var header = document.querySelector('.header');
+    header.classList.toggle('sticky',window.scrollY>100)
+    menubar.classList.remove('bx-x');
+    Navbar.classList.remove('active')
+}
